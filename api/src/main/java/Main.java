226c101f722941d45f727d7f7cb951b1830bc283
wayne0299 <wayne02990@gmail.com>
2024-12-02 21:37:33 +0900

    delivery-service clone coding UserDto 멀티모듈 설정

---
+++ api/src/main/java/Main.java
@@ -0,0 +1,8 @@
+import org.delivery.db.UserDto;
+
+public class Main {
+
+    public static void main(String[] args){
+        UserDto userDto = new UserDto();        // db 프로젝트 하위의 UserDto 를 가져옴.
+    }
+}
