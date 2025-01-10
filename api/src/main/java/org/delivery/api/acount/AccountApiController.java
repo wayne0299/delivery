82e49f779ca3e05bb0d511b63d3b0ff1e7a99363
wayne0299 <wayne02990@gmail.com>
2025-01-10 23:11:24 +0900

    멀티모듈 설정 및 스프링 부트 직접 만들어보기.
    Api Error Code 적용

--- api/src/main/java/org/delivery/api/acount/AccountApiController.java
+++ api/src/main/java/org/delivery/api/acount/AccountApiController.java
@@ -3,14 +3,13 @@ package org.delivery.api.acount;
 import lombok.RequiredArgsConstructor;
 import org.delivery.api.acount.model.AccountMeResponse;
 import org.delivery.api.common.api.Api;
-import org.delivery.api.common.error.UserErrorCode;
-import org.delivery.db.account.AccountEntity;
+
 import org.delivery.db.account.AccountRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletResponse;
+
 import java.time.LocalDateTime;
 
 @RequiredArgsConstructor
