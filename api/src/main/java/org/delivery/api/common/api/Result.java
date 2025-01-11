44d6279bc642117fd254f03713c6f1bf8f02624f
wayne0299 <wayne02990@gmail.com>
2025-01-11 20:17:29 +0900

    멀티모듈 설정 및 스프링 부트 직접 만들어보기.
    Exception Handler 적용

--- api/src/main/java/org/delivery/api/common/api/Result.java
+++ api/src/main/java/org/delivery/api/common/api/Result.java
@@ -28,7 +28,7 @@ public class Result {
         return Result.builder()
                 .resultCode(errorCodeIfs.getErrorCode())
                 .resultMessage(errorCodeIfs.getDescription())
-                .resultDescription("성공")
+                .resultDescription("에러 발생")
                 .build();
     }
 
